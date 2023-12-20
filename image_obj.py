562c31e0d9e89022039c321b6575f045d2fd0594
ronigatt <ronigattegno@mail.tau.ac.il>
2023-12-20 22:59:38 +0200

    .

--- image_obj.py
+++ image_obj.py
@@ -1,4 +1,10 @@
+import cv2
+import tifffile as tiff
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from pathlib import Path
 from PIL import Image
 
@@ -6,41 +12,48 @@ from PIL import Image
 class ImageObj:
 
     def __init__(self, directory_location: Path, image_name: str):
-        green_image_path, red_image_path = self.get_images_path(directory_location, image_name)
-        self.green_channel = self.get_image(green_image_path)
-        self.red_channel = None
+        images_path = self.get_images_path(directory_location, image_name)
+        self.green_channel = self.get_image(images_path[0])
+        self.red_channel = self.get_image(images_path[1])
         self.tumor_location = None
         self.bbbd_location = None
 
-    def get_images_path(self, directory_location: Path, image_name: str):
-        # return images path for green and red channels
-        # directory_location: Path
-        # image_name: str
-        # green_image_path: Path
-        # red_image_path: Path
+    @staticmethod
+    def get_images_path(directory_location: Path, image_name: str) -> list:
+        """
+        return images path for green and red channels
+        directory_location: Path
+        image_name: str
+        green_image_path: Path
+        red_image_path: Path
+        """
         image_path = directory_location / image_name
         files = [x for x in image_path.glob('**/*') if x.is_file()]
         files_names = [files[i].name for i in range(len(files))]
-        green_image_path = [files[loc] for loc, i in enumerate(files_names) if 'FITC' in i]
-        red_image_path = [files[loc] for loc, i in enumerate(files_names) if 'CY' in i]
+        green_image_path = [files[loc] for loc, i in enumerate(files_names) if 'FITC' in i][0]
+        red_image_path = [files[loc] for loc, i in enumerate(files_names) if 'CY' in i][0]
 
-        return green_image_path, red_image_path
+        return [green_image_path, red_image_path]
 
-    def get_image(self, green_image_path: Path):
-        # return image from path
-        # green_image_path: Path
-        # image:
-
-
-
-
+    @staticmethod
+    def get_image(image_path: Path) -> np.ndarray:
         """
-    def find_string(self, list_of_strings: list, string: str):
-        # return index of substring in list of strings
-        for i in range(len(list_of_strings)):
-            if string in list_of_strings[i]:
-                return i
-"""
+        return image from path
+        image_path: Path
+        image: np.ndarray
+        """
+        image = tiff.imread(image_path)[::-1, ::-1].transpose()
+        #image2 = Image.open(image_path)
+        #plt.figure()
+        #plt.imshow(image, cmap='Greys')
+        #plt.show()
+        return image
+
+    def find_tumor(self):
+        pass
+
+
+
 
 
 

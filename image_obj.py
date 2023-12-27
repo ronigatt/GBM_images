3acff711bf1119bfe3a3a7dd7f7826c2e6f16896
ronigatt <ronigattegno@mail.tau.ac.il>
2023-12-27 19:18:57 +0200

    option for merge image

--- image_obj.py
+++ image_obj.py
@@ -13,11 +13,13 @@ class ImageObj:
 
     def __init__(self, directory_location: Path, image_name: str):
         images_path = self.get_images_path(directory_location, image_name)
-        self.green_channel = self.get_image(images_path[0])
-        self.red_channel = self.get_image(images_path[1])
+        self.green_channel = None
+        self.red_channel = None
+        self.get_image(images_path)
+        self.max_val = self.red_channel.max()
         self.tumor_location = None
         self.bbbd_location = None
-        self.max_val = 65535
+
 
     @staticmethod
     def get_images_path(directory_location: Path, image_name: str) -> list:
@@ -31,24 +33,27 @@ class ImageObj:
         image_path = directory_location / image_name
         files = [x for x in image_path.glob('**/*') if x.is_file()]
         files_names = [files[i].name for i in range(len(files))]
-        green_image_path = [files[loc] for loc, i in enumerate(files_names) if 'FITC' in i][0]
-        red_image_path = [files[loc] for loc, i in enumerate(files_names) if 'CY' in i][0]
+        if len ([files[loc] for loc, i in enumerate(files_names) if 'Merge' in i]):
+            image_path = [files[loc] for loc, i in enumerate(files_names) if 'Merge' in i][0]
+        else:
+            green_image_path = [files[loc] for loc, i in enumerate(files_names) if 'FITC' in i][0]
+            red_image_path = [files[loc] for loc, i in enumerate(files_names) if 'CY' in i][0]
+            image_path = [green_image_path, red_image_path]
+        return image_path
 
-        return [green_image_path, red_image_path]
-
-    @staticmethod
-    def get_image(image_path: Path) -> np.ndarray:
+    def get_image(self, image_path: Path or list):
         """
         return image from path
         image_path: Path
         image: np.ndarray
         """
-        image = tiff.imread(image_path)[::-1, ::-1].transpose()
-        #image2 = Image.open(image_path)
-        #plt.figure()
-        #plt.imshow(image, cmap='Greys')
-        #plt.show()
-        return image
+        if isinstance(image_path, list):
+            self.green_channel = tiff.imread(image_path[0])[::-1, ::-1].transpose()
+            self.red_channel = tiff.imread(image_path[1])[::-1, ::-1].transpose()
+        else:
+            image = tiff.imread(image_path)
+            self.green_channel = image[:, :, 1][::-1, ::-1].transpose()
+            self.red_channel = image[:, :, 0][::-1, ::-1].transpose()
 
     def find_roi(self, tumor=False, bbbd=False):
         """
@@ -59,13 +64,30 @@ class ImageObj:
         image = self.green_channel if tumor or not bbbd else self.red_channel
         # convert the image to binary image
         _, image = cv2.threshold(image, 0.1*self.max_val, self.max_val, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-        denoised = cv2.morphologyEx(np.uint8(image), cv2.MORPH_CLOSE, np.ones((21, 21), np.uint8), iterations=13)
+        denoised = cv2.morphologyEx(np.uint8(image), cv2.MORPH_CLOSE, np.ones((7, 7), np.uint8), iterations=11)
         if tumor:
             self.tumor_location = denoised < 255
         if bbbd:
             self.bbbd_location = denoised < 255
 
+    def find_brain(self):
+        """
+        find brain in image
+        """
+        image = self.green_channel
+        # convert the image to binary image
+        _, image = cv2.threshold(image, 254, self.max_val, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        denoised = cv2.morphologyEx(np.uint8(image), cv2.MORPH_CLOSE, np.ones((7, 7), np.uint8), iterations=11)
+        self.brain_location = denoised < 255
 
+    def median_red_intensity(self, roi: np.ndarray) -> float:
+        """
+        return median red intensity in roi
+        roi: np.ndarray
+        median_red_intensity: float
+        """
+        self.red_channel[roi]
+        return np.median(self.red_channel[roi])
 
 
 
--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-
+import matplotlib.pyplot as plt
 from pathlib import Path
 
 from image_obj import ImageObj
 
-
-def print_hi(name):
-    pass
-
-
 if __name__ == '__main__':
-    directory_location = Path(r'G:\My Drive\Ilovitsh Lab\Export images')
-    image_name = 'GBM4_S3_3_fl'
-    image = ImageObj(directory_location, image_name)
-    image.find_roi(tumor=True)
-    image.find_roi(bbbd=True)
+    directory_location = Path(r'G:\My Drive\Ilovitsh Lab\Export images\with_merge')
+    images_names = [x.name for x in directory_location.glob('**/*') if x.is_dir()]
+    tumor_median_red_intensity = [0] * len(images_names)
+    bbbd_median_red_intensity = [0] * len(images_names)
+    for i, image_name in enumerate(images_names):
+        image = ImageObj(directory_location, image_name)
+        image.find_roi(tumor=True)
+        image.find_roi(bbbd=True)
+        tumor_median_red_intensity[i] = image.median_red_intensity(image.tumor_location)
+        only_bbbd_location = image.bbbd_location * ~image.tumor_location
+        bbbd_median_red_intensity[i] = image.median_red_intensity(only_bbbd_location)
+        #image.find_brain()
+
+    print(tumor_median_red_intensity)
+    print(bbbd_median_red_intensity)
+    '''
+    plt.figure()
+    plt.imshow(image.red_channel)
+    plt.figure()
+    plt.imshow(image.bbbd_location)
+    '''
+
 
